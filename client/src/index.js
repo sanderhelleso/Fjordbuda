3d809614d55cecf3403cbbfb112ec5536105ed59
Sanderhelleso <sanderhelleso@hotmail.com>
2018-07-28 16:30:19 +0200

    modified size for landing img

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
 
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'mdbreact/dist/css/mdb.css';
+
 import App from './components/App';
 
 ReactDOM.render(
