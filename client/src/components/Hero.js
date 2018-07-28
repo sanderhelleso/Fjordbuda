361c31e4b2ee5cd12f40749538901c8b8a856041
Sanderhelleso <sanderhelleso@hotmail.com>
2018-07-28 13:22:52 +0200

    added hero

---
+++ client/src/components/Hero.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Hero extends Component {
+    render() {
+        return (
+            <div id="hero">
+                <div className="container">
+                    <h1>Vinjenaustet</h1>
+                    <p id="heroText">stiftet i den lille, idylliske landsbyen Geiranger. Vinjenaustet startet som et lite naust i 1993 og har siden voskt til å bli en av de største leverandørene innen norske klær og suvenirer</p>
+                </div>
+            </div>
+        )
+    }
+}
