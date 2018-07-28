dc3085b847fc841b2f82c4b7631e753c7f491a85
Sanderhelleso <sanderhelleso@hotmail.com>
2018-07-28 12:38:39 +0200

    added react materialize

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 
-export default class App extends Component {
+export default class Landing extends Component {
     render() {
         return (
             <div>
-                <h1>Vinjenaustet</h1>
+                
             </div>
         )
     }
