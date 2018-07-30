d749ee71e59cce659ef412c5124b4aa0b025802a
Sanderhelleso <sanderhelleso@hotmail.com>
2018-07-31 01:40:56 +0200

    added loadingscreen animation

--- client/src/components/Loading.js
+++ client/src/components/Loading.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class Loading extends Component {
     render() {
         return (
             <div id='loadingScreen'>
-                <div>
-                    <h1>Hello World</h1>
+                <div id='loadingHero'>
+                    <h1 data-hover="Vinjenaustet">Vinjenaustet</h1>
                 </div>
+                <div class="lds-ripple"><div></div><div></div></div>
             </div>
         )
     }
