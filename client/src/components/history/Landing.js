93e498d6d433974e74e4ce237ec3dab6d9f3dfdd
Sanderhelleso <sanderhelleso@hotmail.com>
2018-08-01 20:58:59 +0200

    added dynamic animation for arrow history section

--- client/src/components/history/Landing.js
+++ client/src/components/history/Landing.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { ArrowDown } from 'react-feather';
 
 export default class Landing extends Component {
     componentDidMount() {
         window.addEventListener('scroll', scrollSpy);
+        animateArrow();
     }
 
     render() {
@@ -11,7 +13,8 @@ export default class Landing extends Component {
                 <header id='historyHeader'>
                     <h1>En historie uten like</h1>
                     <p>Stiftet i den idylliske landsbyen Geiranger, Vinjenaustet er ikke som alle andre naust</p>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
+                    <span id='readMoreBtn'><ArrowDown size={40} color='#ffffff' /></span>
+                    <svg id='headerSvg' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
                         <polygon className="svg--sm" fill="white" points="0,0 30,100 65,21 90,100 100,75 100,100 0,100"/>
                         <polygon className="svg--lg" fill="white" points="0,0 15,100 33,21 45,100 50,75 55,100 72,20 85,100 95,50 100,80 100,100 0,100" />
                     </svg>
@@ -37,4 +40,14 @@ function scrollSpy() {
         navbar.classList.remove("topNavOffBg");
         navbar.classList.add("topNavOnBg");
     }
+}
+
+function animateArrow() {
+    const arrow = document.querySelector("#readMoreBtn");
+    setInterval(() => {
+        arrow.className = 'animated fadeInDown';
+        setTimeout(() => {
+            arrow.className = '';
+        }, 1500);
+    }, 4000);
 }
