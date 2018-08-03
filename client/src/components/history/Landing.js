29801d68194bf3c41534ba7e69a3430ba27d4b08
Sanderhelleso <sanderhelleso@hotmail.com>
2018-08-03 20:59:05 +0200

    added fadein react libary

--- client/src/components/history/Landing.js
+++ client/src/components/history/Landing.js
@@ -21,16 +21,12 @@ export default class Landing extends Component {
         return (
             <div>
                 <div id='progressBar' className='z-depth-1' />
-                <header id='historyHeader'>
+                <header id='historyHeader' className='z-depth-2'>
                     <div id='historyIntro' className='container'>
                         <h1>En historie uten like</h1>
                         <p>Stiftet i den idylliske landsbyen Geiranger, Vinjenaustet er ikke som alle andre naust</p>
                     </div>
-                    <span id='readMoreBtn'><ArrowDown size={40} color='#ffffff' /></span>
-                    <svg id='headerSvg' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
-                        <polygon className="svg--sm" fill="white" points="0,0 30,100 65,21 90,100 100,75 100,100 0,100"/>
-                        <polygon className="svg--lg" fill="white" points="0,0 15,100 33,21 45,100 50,75 55,100 72,20 85,100 95,50 100,80 100,100 0,100" />
-                    </svg>
+                    <span id='readMoreBtn'><img src='img/history/scroll.png' /></span>
                 </header>
                 <Story />
             </div>
--- client/src/components/history/Story.js
+++ client/src/components/history/Story.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Row, Col } from 'react-materialize';
+import Zoom from 'react-reveal/Zoom';
+import Fade from 'react-reveal/Fade';
 
 export default class Story extends Component {
     render() {
@@ -19,20 +21,25 @@ export default class Story extends Component {
                                 <img className='responsive-img z-depth-1' src='img/menu/menubg1.jpg' />
                                 <h5>Dette er et bilde</h5>
                             </div>
+                            
+                            <Zoom>
+                                <h2>Det var engang...</h2>
+                            </Zoom>
 
-                            <h2>Det var engang...</h2>
-                            <div className='part'>
-                                <div className='partId'>
-                                        <h3>02</h3>
-                                </div>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mollis ex placerat lacus porta, eget lacinia urna venenatis. Fusce convallis leo vel ipsum laoreet, eget gravida neque pulvinar. Vestibulum dictum a purus quis malesuada. Etiam eu lacus id nibh eleifend congue. Phasellus interdum accumsan dictum. Mauris quis congue lorem, nec semper dui. Praesent ipsum augue, finibus sed ornare eget, pretium ut mauris. Phasellus consequat nibh vitae leo porttitor, eget efficitur nibh varius. Fusce enim neque, pulvinar eu odio quis, porta auctor sapien.
-                                    
-                                    
+                            <Fade>
+                                <div className='part'>
+                                    <div className='partId'>
+                                            <h3>02</h3>
+                                    </div>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mollis ex placerat lacus porta, eget lacinia urna venenatis. Fusce convallis leo vel ipsum laoreet, eget gravida neque pulvinar. Vestibulum dictum a purus quis malesuada. Etiam eu lacus id nibh eleifend congue. Phasellus interdum accumsan dictum. Mauris quis congue lorem, nec semper dui. Praesent ipsum augue, finibus sed ornare eget, pretium ut mauris. Phasellus consequat nibh vitae leo porttitor, eget efficitur nibh varius. Fusce enim neque, pulvinar eu odio quis, porta auctor sapien.
+                                        
+                                        
                                     Integer condimentum dolor et ipsum vulputate, non suscipit lacus maximus. Quisque volutpat placerat ante, ut tempor nisl rhoncus id. Maecenas enim erat, mattis a pulvinar in, volutpat eget turpis. Duis vel lacus efficitur velit ultricies cursus in vitae arcu. Curabitur turpis lacus, dapibus in facilisis a, dapibus quis felis. Ut euismod ligula sed dolor consectetur lacinia. Proin malesuada lacus lectus, sit amet elementum est auctor non. Phasellus pharetra orci nunc, quis euismod diam rutrum eu. Proin id urna eu dui tincidunt mattis vitae id felis. Nulla tempor tortor ac nisi laoreet varius. Morbi turpis nulla, aliquam sed lorem eu, bibendum consectetur ante. Duis scelerisque nulla eu mi dignissim malesuada. Vestibulum tellus turpis, ullamcorper eu faucibus in, aliquet tempus nulla. Curabitur fermentum gravida rutrum.</p>
                                     <img className='responsive-img z-depth-1' src='img/menu/menubg2.jpg' />
                                     <h5>Dette er et bilde</h5>
-                            </div>
-
+                                </div>
+                            </Fade>
+                            
                             <h2>Det var engang...</h2>
                             <div className='part'>
                                 <div className='partId'>
