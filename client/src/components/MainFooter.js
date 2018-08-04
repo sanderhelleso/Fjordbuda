6a3b0bcf0cb0551533425d9e4b8d00a884baa85e
Sanderhelleso <sanderhelleso@hotmail.com>
2018-08-04 23:42:39 +0200

    added more cols and copyright

--- client/src/components/MainFooter.js
+++ client/src/components/MainFooter.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Row, Col } from 'react-materialize';
+import { Navigation, MessageCircle } from 'react-feather';
 
 export default class MainFooter extends Component {
     render() {
@@ -7,17 +8,46 @@ export default class MainFooter extends Component {
             <footer>
                 <div className='container'>
                     <Row id='footerRow'>
-                        <Col l={3} m={6} s={12}>
-                            <h5>Språk</h5>
+                        <Col id='languages' l={3} m={6} s={12}>
+                            <ul>
+                                <li><a><Navigation size={14} className='mr-2'/> Norway</a></li>
+                                <li><a><MessageCircle size={14}className='mr-2'/> English</a></li>
+                                <li className='space'></li>
+                                <li className='copyright'>© Vinjenaustet</li>
+                            </ul>
                         </Col>
                         <Col l={3} m={6} s={12}>
                             <h5>Butikker</h5>
+                            <ul>
+                                <li><a>qweqeq</a></li>
+                                <li><a>qweqeq</a></li>
+                                <li><a>qweqeq</a></li>
+                                <li><a>qweqeq</a></li>
+                                <li><a>qweqeq</a></li>
+                                <li><a>qweqeq</a></li>
+                            </ul>
                         </Col>
                         <Col l={3} m={6} s={12}>
                             <h5>Selskapet</h5>
+                            <ul>
+                                <li><a>qweqeq</a></li>
+                                <li><a>qweqeq</a></li>
+                                <li><a>qweqeq</a></li>
+                                <li><a>qweqeq</a></li>
+                                <li><a>qweqeq</a></li>
+                                <li><a>qweqeq</a></li>
+                            </ul>
                         </Col>
                         <Col l={3} m={6} s={12}>
                             <h5>Kontakt</h5>
+                            <ul>
+                                <li><a>qweqeq</a></li>
+                                <li><a>qweqeq</a></li>
+                                <li><a>qweqeq</a></li>
+                                <li><a>qweqeq</a></li>
+                                <li><a>qweqeq</a></li>
+                                <li><a>qweqeq</a></li>
+                            </ul>
                         </Col>
                     </Row>
                 </div>
--- client/src/components/history/History.js
+++ client/src/components/history/History.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Parallax } from 'react-materialize';
 
 import Landing from './Landing';
 
