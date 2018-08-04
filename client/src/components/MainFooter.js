6a381dd4af2bc3b2b1fe67ed92e0464ec999040f
Sanderhelleso <sanderhelleso@hotmail.com>
2018-08-04 21:36:32 +0200

    added materialize footer

--- client/src/components/MainFooter.js
+++ client/src/components/MainFooter.js
@@ -1,11 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Footer } from 'react-materialize';
 
 export default class MainFooter extends Component {
     render() {
         return (
-        <div>
-            <h3>Footer content</h3>
-        </div>
+            <Footer copyrights="&copy 2015 Copyright Text"
+                moreLinks={
+                <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
+                }
+                links={
+                <ul>
+                    <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                    <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                    <li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                    <li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                </ul>
+                }
+                className='example'
+            >
+              <h5 className="white-text">Footer Content</h5>
+              <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+            </Footer>
         )
     }
 }
