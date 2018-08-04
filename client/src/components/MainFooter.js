9d30d876fd09edfcb996e7fec7da5053bae251f7
Sanderhelleso <sanderhelleso@hotmail.com>
2018-08-04 22:44:30 +0200

    added grid row and cols

--- client/src/components/MainFooter.js
+++ client/src/components/MainFooter.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
-import { Footer } from 'react-materialize';
+import { Row, Col } from 'react-materialize';
 
 export default class MainFooter extends Component {
     render() {
         return (
-            <Footer copyrights="&copy 2015 Copyright Text"
-                moreLinks={
-                <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
-                }
-                links={
-                <ul>
-                    <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
-                    <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
-                    <li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
-                    <li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>
-                </ul>
-                }
-                className='example'
-            >
-              <h5 className="white-text">Footer Content</h5>
-              <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
-            </Footer>
+            <footer>
+                <div className='container'>
+                    <Row id='footerRow'>
+                        <Col l={3} m={6} s={12}>
+                            <h5>Språk</h5>
+                        </Col>
+                        <Col l={3} m={6} s={12}>
+                            <h5>Butikker</h5>
+                        </Col>
+                        <Col l={3} m={6} s={12}>
+                            <h5>Selskapet</h5>
+                        </Col>
+                        <Col l={3} m={6} s={12}>
+                            <h5>Kontakt</h5>
+                        </Col>
+                    </Row>
+                </div>
+            </footer>
         )
     }
 }
--- client/src/components/history/History.js
+++ client/src/components/history/History.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Parallax } from 'react-materialize';
 
 import Landing from './Landing';
 
