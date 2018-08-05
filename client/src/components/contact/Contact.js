1639dfdb4541f886ed9652650d352ab7ffff0e0a
Sanderhelleso <sanderhelleso@hotmail.com>
2018-08-05 21:32:12 +0200

    added img and heading for contact section

--- client/src/components/contact/Contact.js
+++ client/src/components/contact/Contact.js
@@ -6,15 +6,23 @@ export default class Contact extends Component {
         document.title = '✉️ Kontakt | Fjordbuda';
     }
 
+    componentDidMount() {
+        console.log(document.querySelector('#navLogo'));
+        setTimeout(() => {
+            document.querySelector('#navLogo').src = 'img/logo/Vinje1_white.png';
+        }, 100);
+    }
+
     render() {
         return (
         <div id='contact'>
             <Row>
-                <div className='col col-sm-12 col-md-6 col-lg-6'>
-                    <Parallax imageSrc="http://materializecss.com/images/parallax1.jpg"/>
+                <div id='contactImg' className='col col-sm-12 col-md-7 col-lg-7 z-depth-5'>
                 </div>
-                <div className='col col-sm-12 col-md-6 col-lg-6'>
-                    qweqwe
+                <div className='col col-sm-12 col-md-5 col-lg-5'>
+                    <div id='contactCont'>
+                        <h1>Kontakt</h1>
+                    </div>
                 </div>
             </Row>
         </div>
