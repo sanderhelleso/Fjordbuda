1f2d82d563acdf69e46cd65989a1358be879be22
Sanderhelleso <sanderhelleso@hotmail.com>
2018-08-06 20:51:26 +0200

    added main menu for lookbook

--- client/src/components/lookbook/Lookbook.js
+++ client/src/components/lookbook/Lookbook.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import { Row } from 'react-materialize';
+import { Minus, ArrowDown } from 'react-feather';
 
 export default class Lookbook extends Component {
 
     componentDidMount() {
-        //document.querySelector('#topNav').style.position = 'absolute';
         document.title = `🔥 Lookbook ${new Date().getFullYear()} | Fjordbuda`;
     }
 
@@ -14,7 +14,19 @@ export default class Lookbook extends Component {
                 <Row>
                     <div id='lookBookCont' className='col col-sm-8 col-md-6 col-lg-7'>
                         <div id='lookBookMenu' className='container'>
-                            <h5>Velg ditt merke:</h5>
+                            <h5>Velg ditt merke</h5>
+                            <Minus color='#363F54' />
+                            <div id='lookBookCategories'>
+                                <h2><span>01.</span>Klesmerke</h2>
+                                <h2><span>02.</span>Klesmerke</h2>
+                                <h2><span>03.</span>Klesmerke</h2>
+                                <h2><span>04.</span>Klesmerke</h2>
+                                <h2><span>05.</span>Klesmerke</h2>
+                            </div>
+                            <h5>Eller se alle</h5>
+                            <Minus color='#363F54' />
+                            <br />
+                            <ArrowDown id='arrowDown' color='#363F54' />
                         </div>
                     </div>
                     <div id='lookBookImg' className='col col-sm-4 col-md-6 col-lg-5 z-depth-5'>
