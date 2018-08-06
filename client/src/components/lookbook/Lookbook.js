e8963558e82e8cf992aef8d36b21688c3fc62bd2
Sanderhelleso <sanderhelleso@hotmail.com>
2018-08-06 18:23:30 +0200

    renamed css to lookbook

--- client/src/components/lookbook/Lookbook.js
+++ client/src/components/lookbook/Lookbook.js
@@ -3,13 +3,14 @@ import React, { Component } from 'react'
 export default class Lookbook extends Component {
 
     componentDidMount() {
-        document.querySelector('#topNav').style.position = 'absolute';
+        //document.querySelector('#topNav').style.position = 'absolute';
     }
 
     render() {
         return (
-            <div id='merch' className='container'>
-                <h1>wqeqwe</h1>
+            <div id='lookBook'>
+                <div className='container'>
+                </div>
             </div>
         )
     }
