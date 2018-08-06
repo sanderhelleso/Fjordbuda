e0006ef77e23e54e88a9f03dc1e85f3daa7e2871
Sanderhelleso <sanderhelleso@hotmail.com>
2018-08-06 19:51:00 +0200

    added lookbook main style

--- client/src/components/lookbook/Lookbook.js
+++ client/src/components/lookbook/Lookbook.js
@@ -5,6 +5,7 @@ export default class Lookbook extends Component {
 
     componentDidMount() {
         //document.querySelector('#topNav').style.position = 'absolute';
+        document.title = `🔥 Lookbook ${new Date().getFullYear()} | Fjordbuda`;
     }
 
     render() {
@@ -12,12 +13,15 @@ export default class Lookbook extends Component {
             <div id='lookBook'>
                 <Row>
                     <div id='lookBookCont' className='col col-sm-8 col-md-6 col-lg-7'>
-                        <div className='container'>
+                        <div id='lookBookMenu' className='container'>
+                            <h5>Velg ditt merke:</h5>
                         </div>
                     </div>
                     <div id='lookBookImg' className='col col-sm-4 col-md-6 col-lg-5 z-depth-5'>
                         <div id='lookBookHeader'>
                             <h1>Lookbook</h1>
+                            <h3>Høst / Vinter</h3>
+                            <h2 id='lookBookYear'>{new Date().getFullYear()}</h2>
                         </div>
                     </div>
                 </Row>
