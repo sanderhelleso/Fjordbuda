0c9f3a08cc036b97f8ec8c01ecd6c5401990e8c6
Sanderhelleso <sanderhelleso@hotmail.com>
2018-08-06 19:19:50 +0200

    added lookbook header

--- client/src/components/lookbook/Lookbook.js
+++ client/src/components/lookbook/Lookbook.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Row } from 'react-materialize';
 
 export default class Lookbook extends Component {
 
@@ -9,8 +10,17 @@ export default class Lookbook extends Component {
     render() {
         return (
             <div id='lookBook'>
-                <div className='container'>
-                </div>
+                <Row>
+                    <div id='lookBookCont' className='col col-sm-8 col-md-6 col-lg-7'>
+                        <div className='container'>
+                        </div>
+                    </div>
+                    <div id='lookBookImg' className='col col-sm-4 col-md-6 col-lg-5 z-depth-5'>
+                        <div id='lookBookHeader'>
+                            <h1>Lookbook</h1>
+                        </div>
+                    </div>
+                </Row>
             </div>
         )
     }
