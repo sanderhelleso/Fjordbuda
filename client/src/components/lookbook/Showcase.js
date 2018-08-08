8215e65f7a3cb2737e6c41a09de5277af703c36a
Sanderhelleso <sanderhelleso@hotmail.com>
2018-08-09 00:38:11 +0200

    added logo and intro to showcase component

--- client/src/components/lookbook/Showcase.js
+++ client/src/components/lookbook/Showcase.js
@@ -13,8 +13,12 @@ export default class Showcase extends Component {
             <div id='showcase' className='container'>
                 <div id='showcaseHero'>
                     <h1>{this.getBrand()}</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec maximus quis urna in suscipit. Fusce quis molestie nunc, non facilisis dui. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc convallis ante dolor. Sed quis imperdiet arcu. Quisque pellentesque vel sapien in molestie.</p>
+                    <img id='brandLogo' src={`../img/lookbook/${window.location.href.split('/')[4]}/logo.png`} />
                 </div>
-                <Gallery />
+                <div id='galleryCont'>
+                    <Gallery />
+                </div>
             </div>
         )
     }
