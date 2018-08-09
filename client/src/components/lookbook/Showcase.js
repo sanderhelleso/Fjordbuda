344c4313b2c340a744e3f4182b63c2478bfa3eb8
Sanderhelleso <sanderhelleso@hotmail.com>
2018-08-09 18:28:33 +0200

    added function to uppercase each word in title

--- client/src/components/lookbook/Showcase.js
+++ client/src/components/lookbook/Showcase.js
@@ -4,15 +4,15 @@ import Gallery from './ClothingGallery';
 
 export default class Showcase extends Component {
 
-    getBrand() {
-        return window.location.href.split('/')[4].split('-').join(' ');
+    componentWillMount() {
+        document.title = `${toTitleCase(url())} | Lookbook | Fjordbuda`;
     }
 
     render() {
         return (
             <div id='showcase' className='container'>
                 <div id='showcaseHero'>
-                    <h1>{this.getBrand()}</h1>
+                    <h1>{url()}</h1>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec maximus quis urna in suscipit. Fusce quis molestie nunc, non facilisis dui. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc convallis ante dolor. Sed quis imperdiet arcu. Quisque pellentesque vel sapien in molestie.</p>
                     <img id='brandLogo' src={`../img/lookbook/${window.location.href.split('/')[4]}/logo.png`} />
                 </div>
@@ -22,4 +22,14 @@ export default class Showcase extends Component {
             </div>
         )
     }
+}
+
+function url() {
+    return window.location.href.split('/')[4].split('-').join(' ');
+}
+
+function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function(txt){
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
 }
