1e44759efe465b21ffed7d8e7bc8971934f69acf
Sanderhelleso <sanderhelleso@hotmail.com>
2018-08-11 18:21:46 +0200

    implemented array flattener before sending

--- routes/gallery.js
+++ routes/gallery.js
@@ -4,9 +4,37 @@ module.exports = app => {
 
     // fetch all gallery imgs
     app.post('/api/gallery/all', (req, res) => {
-        res.send({
-            galleryImgs: 123
-        });
+
+        const imgs = [];
+        let galleryCount = 0;
+        let folderCount = 0;
+        const url = `./client/public/img/lookbook/`;
+        readdir(url)
+        .then(folders => {
+            folders.forEach(folder => {
+                folderCount++;
+                const galleryPath = `${url}${folder}/gallery`;
+                readdir(galleryPath)
+                .then(files => {
+                    imgs.push(files);
+                    galleryCount++;
+                })
+                .catch(err => {
+                    // no folder is present, reduce count
+                    folderCount--;
+                    return;
+                })
+                .then(done => {
+
+                    // send AFTER all data is loaded into array
+                    if (galleryCount === folderCount) {
+                        res.send({
+                            galleryImgs: [].concat.apply([], imgs) // flatten array
+                        });
+                    }
+                });
+            });
+        })
     }); 
 
     // fetch brand specific gallery imgs
