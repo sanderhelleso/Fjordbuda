9c12915f5e98c731c180d57683a9b2253bd2c62c
Sanderhelleso <sanderhelleso@hotmail.com>
2018-08-11 22:41:26 +0200

    modified btn

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -7,7 +7,7 @@ export default class Hero extends Component {
                 <div className="container animated fadeInUp noSelect">
                     <img src='../img/logo/FjordB_2.png' />
                     <p id="heroText">stiftet i den lille, idylliske landsbyen Geiranger. Vinjenaustet startet som et lite naust i 1993 og har siden voskt til å bli en av de største leverandørene innen norske klær og suvenirer</p>
-                    <button id='explore' className='btn'>Utforsk buda</button>
+                    <button id='explore' className='btn z-depth-5'>Utforsk buda</button>
                 </div>
             </div>
         )
