b69ee6c79d609ca95ac059f62a000d05a5789917
Sanderhelleso <sanderhelleso@hotmail.com>
2018-08-15 00:48:31 +0200

    added multiple icons

--- client/src/components/stores/Stores.js
+++ client/src/components/stores/Stores.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { MapPin, ChevronDown } from 'react-feather';
 
 export default class Stores extends Component {
     render() {
@@ -6,12 +7,15 @@ export default class Stores extends Component {
             <div>
                 <header id='storesHeader'>
                     <div id='selectStore' className='container'>
-                        <div id='selectStoreRow' className='row'>
+                        <div id='selectStoreRow' className='row z-depth-5'>
                             <div className='col s6 findStore'>
                                 <h5>Finn din Fjordbuda</h5>
                             </div>
-                            <div className='col s6'>
-                                <h5>Velg sted</h5>
+                            <div className='col s6 selectStoreOption'>
+                                <h5><MapPin size={22} /> Velg sted</h5>
+                                <span>
+                                    <ChevronDown size={22} color={'#9e9e9e'} />
+                                </span>
                             </div>
                         </div>
                     </div>
