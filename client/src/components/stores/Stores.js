14676bab14855a622057ea132e6fba6edb6e9ec4
Sanderhelleso <sanderhelleso@hotmail.com>
2018-08-15 00:37:52 +0200

    added base srtling for find store component

--- client/src/components/stores/Stores.js
+++ client/src/components/stores/Stores.js
@@ -5,6 +5,16 @@ export default class Stores extends Component {
         return (
             <div>
                 <header id='storesHeader'>
+                    <div id='selectStore' className='container'>
+                        <div id='selectStoreRow' className='row'>
+                            <div className='col s6 findStore'>
+                                <h5>Finn din Fjordbuda</h5>
+                            </div>
+                            <div className='col s6'>
+                                <h5>Velg sted</h5>
+                            </div>
+                        </div>
+                    </div>
                 </header>
                 <section>
                     
