c9ea98409dc17317610ca1e4d17a727e252d3980
Sanderhelleso <sanderhelleso@hotmail.com>
2018-08-19 16:31:11 +0200

    now renders specific bg from funciton

--- client/src/components/stores/SelectedStore.js
+++ client/src/components/stores/SelectedStore.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import StoreInfo from './StoreInfo';
 import { renderStoreTitle } from '../../globals/renderTitle';
-import { getUrl } from '../../globals/getUrl'; 
 
 export default class SelectedStore extends Component {
 
     componentDidMount() {
-        console.log(getUrl());
         document.querySelector('footer').style.marginTop = '70vh';
+        this.setBg(renderStoreTitle().split(' ').join('-').toLowerCase());
+    }
 
+    setBg(bgPath) {
+        console.log(bgPath);
         const headerBg = document.querySelector('#selectedStoreHeader');
-        headerBg.style.background = `linear-gradient(rgba(121, 85, 72, 0.9), rgba(0, 0, 0, 0.7)),url("../img/stores/${getUrl()[1]}.jpg") no-repeat center`;
+        headerBg.style.background = `linear-gradient(rgba(121, 85, 72, 0.9), rgba(0, 0, 0, 0.7)),url("../img/stores/${bgPath}.jpg") no-repeat center`;
         headerBg.style.backgroundSize = 'cover'
     }
 
