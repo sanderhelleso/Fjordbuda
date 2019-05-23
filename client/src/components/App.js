27e8c46429e4f404aedeb0e97727d3aa48f1432a
Sanderhelleso <sanderhelleso@hotmail.com>
2019-05-23 23:09:34 +0200

    Refactored routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -16,83 +16,28 @@ import Showcase from './lookbook/Showcase';
 import Stores from './stores/Stores';
 import SelectedStore from './stores/SelectedStore';
 
-export default class App extends Component {
-    constructor(props) {
-        super(props);
-    }
+const App = () => {
+    return (
+        <div id='main'>
+            <BrowserRouter>
+                <div>
+                    <Route path ='/' component={TopNav} />
+                    <Route exact path='/' component={Landing} />
+                    <Route exact path='/*' component={Loading} />
 
-    componentWillMount() {
-        this.setLang();
-    }
+                    <Route exact path='/butikker' component={Stores} />
+                    <Route exact path='/butikker/*' component={SelectedStore} />
+                    <Route exact path='/lookbook' component={Lookbook} />
+                    <Route exact path='/lookbook/*' component={Showcase} />
+                    <Route exact path='/historie' component={History} />
+                    <Route exact path='/kontakt' component={Contact} />
 
 
-    // render NOR or EN pages depending on browser
-    setLang() {
-        const lang = navigator.language || navigator.userLanguage;
-
-        // check if norwegian browser
-        if (lang === 'no' && getUrl()[0] != 'en') {
-            this.setState({
-                lang: 'no'
-            });
-        }
-
-        else {
-            this.setState({
-                lang: 'en'
-            });
-        }
-    }
-
-    render() {
-        if (this.state.lang === 'no') {
-            // norwegian routing
-            return (
-                <div id='main'>
-                    <BrowserRouter>
-                        <div>
-                            <Route path ='/' component={TopNav} />
-                            <Route exact path='/' component={Landing} />
-                            <Route exact path='/*' component={Loading} />
-    
-                            <Route exact path='/butikker' component={Stores} />
-                            <Route exact path='/butikker/*' component={SelectedStore} />
-                            <Route exact path='/lookbook' component={Lookbook} />
-                            <Route exact path='/lookbook/*' component={Showcase} />
-                            <Route exact path='/historie' component={History} />
-                            <Route exact path='/kontakt' component={Contact} />
-    
-    
-                            <Route path ='/' component={MainFooter} />
-                        </div>
-                    </BrowserRouter>
+                    <Route path ='/' component={MainFooter} />
                 </div>
-            )
-        }
-
-        // english routing
-        else {
-            return (
-                <div id='main'>
-                    <BrowserRouter>
-                        <div>
-                            <Route path ='/en' component={TopNav} />
-                            <Route exact path='/en' component={Landing} />
-                            <Route exact path='/*' component={Loading} />
-    
-                            <Route exact path='/en/stores' component={Stores} />
-                            <Route exact path='/en/butikker/*' component={SelectedStore} />
-                            <Route exact path='/en/lookbook' component={Lookbook} />
-                            <Route exact path='/en/lookbook/*' component={Showcase} />
-                            <Route exact path='/en/history' component={History} />
-                            <Route exact path='/en/contact' component={Contact} />
-    
-    
-                            <Route path ='/en' component={MainFooter} />
-                        </div>
-                    </BrowserRouter>
-                </div>
-            )
-        }
-    }
+            </BrowserRouter>
+        </div>
+    )
 }
+
+export default App;
