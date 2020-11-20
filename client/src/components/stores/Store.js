993ee31369162bb6715d73b4d0cc182f7f0d5b83
Tarald Johansen <tarald.johansen@outlook.com>
2020-11-20 17:43:14 +0100

    update info

--- client/src/components/stores/Store.js
+++ client/src/components/stores/Store.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { getStores } from '../../globals/stores';
 import Row from 'react-materialize/lib/Row';
-import { MapPin, Phone, Mail } from 'react-feather';
+import { MapPin, Phone, Mail, Facebook, Instagram } from 'react-feather';
 
 export const Store = () => {
     const renderStores = () => {
@@ -17,11 +17,31 @@ export const Store = () => {
                         </li>
                         {}
                         <li style={{ paddingTop: '0.5rem' }}>
-                            <Phone /> {store.tlf === '' ? '-' : store.tlf}
+                            <Phone />{' '}
+                            {store.tlf === '' ? (
+                                '-'
+                            ) : (
+                                <a href={`tel:${store.tlf}`}>{store.tlf}</a>
+                            )}
                         </li>
                         <li style={{ paddingTop: '0.5rem' }}>
-                            <Mail /> {store.mail === '' ? '-' : store.mail}
+                            <Mail />{' '}
+                            {store.mail === '' ? (
+                                '-'
+                            ) : (
+                                <a href={`mailto:${store.mail}`}>
+                                    {store.mail}
+                                </a>
+                            )}
                         </li>
+                        {/* <li>
+                            <a href={store.facebook}>
+                                <Facebook />
+                            </a>
+                            <a href={store.instagram}>
+                                <Instagram />
+                            </a>
+                        </li> */}
                     </ul>
                 </div>
             );
